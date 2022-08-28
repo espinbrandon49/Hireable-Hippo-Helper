bafcd9e8dd63504c9962cd897dbaaa19bad2de37
Brice Veyna <bricev99@gmail.com>
2022-08-28 15:22:44 -0700

    Fix milestones reference, formatting

--- project_3/server/models/Application.js
+++ project_3/server/models/Application.js
@@ -1,68 +1,59 @@
 const { Schema, model } = require('mongoose');
 
-
 const applicationSchema = new Schema({
   
-    
   salary: {
     type: Number,
     required: true
   },
   dateApplied: {
     type: Date,
-  
   },
-  contact: {
-    
-    name: {
-      type: String,
-      
-    },
-    emailAddress: {
-      type: String,
-      
-    },
-    phone: {
-      type: String,
-    },
-  
+  contactName: {
+    type: String,
   },
-appliedFrom: {
-  type: String,
-  enum: ['LinkedIn', 'Indeed', 'AngelList', 'Handshake', 'Monster', 'Zip Recruiter', 'Job Fair', 'Company Website', 'Craigslist', 'Other'],
-  required : true 
-},
-jobURL: {
-  type: String,
-  required: true
-},
-jobDescription: {
-  type: String,
-  required: true
-},
-location: {
-  type: String,
-  required: true
-},
-jobType: {
-  type: String,
-  enum: ['In person', 'Hybrid', 'Remote'],
-  required: true
-},
-
-currentMilestone: {
-  type: String,
-  enum: ['Applied', 'Phone Interview', 'Technical Interview', 'In Person Interview', 'Job Offer', 'Accepted', 'Rejected', 'Hippo Donations'],
-  required: true
-},
-milestones: {
-  type: Array,
-},
-
-note: {
-  type: String
-},
-
+  contactEmail: {
+    type: String,
+  },
+  contactPhone: {
+    type: String,
+  },
+  appliedFrom: {
+    type: String,
+    enum: ['LinkedIn', 'Indeed', 'AngelList', 'Handshake', 'Monster', 'Zip Recruiter', 'Job Fair', 'Company Website', 'Craigslist', 'Other'],
+    required : true 
+  },
+  jobURL: {
+    type: String,
+    required: true
+  },
+  jobDescription: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  jobType: {
+    type: String,
+    enum: ['In person', 'Hybrid', 'Remote'],
+    required: true
+  },
+  currentMilestone: {
+    type: String,
+    enum: ['Applied', 'Phone Interview', 'Technical Interview', 'In Person Interview', 'Job Offer', 'Accepted', 'Rejected', 'Hippo Donations'],
+    required: true
+  },
+  milestones: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Milestone',
+    }
+  ],
+  note: {
+    type: String
+  },
 });
 
 const Application = model('Application', applicationSchema);

b58887ef6d4da619f9ef62e36ab268cfec87b350
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-31 15:55:16 -0700

    update config

--- project_3/server/config/connection.js
+++ project_3/server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-//heroku deploy
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost:27017/Hireable-Hippo-Helper',
   {
@@ -9,18 +8,5 @@ mongoose.connect(
   }
 );
 
-//BEFORE HEROKU DEPLOY
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Hireable-Hippo-Helper',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-
-// mongoose.connect('mongodb://127.0.0.1:27017/Hireable-Hippo-Helper',function(){
-//     /* Drop the DB */
-//     mongoose.connection.db.dropDatabase();
-// });
 
 module.exports = mongoose.connection;

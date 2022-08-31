ea9f127826e0933a559d68fe195ec97c72601cad
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-31 13:46:56 -0700

    add milestone

--- project_3/server/seeders/seed.js
+++ project_3/server/seeders/seed.js
@@ -11,13 +11,12 @@ db.once('open', async () => {
     await Milestone.deleteMany({});
 
     await User.create(userSeeds);
-    await Application.create(applicationSeeds);
-    await Milestone.create(milestoneSeeds);
+    // await Milestone.create(milestoneSeeds);
 
     for (let i = 0; i < applicationSeeds.length; i++) {
       const { _id, applicant } = await Application.create(applicationSeeds[i]);
       const user = await User.findOneAndUpdate(
-        {username: applicant},
+        { username: applicant },
         {
           $addToSet: {
             applications: _id,
@@ -26,16 +25,20 @@ db.once('open', async () => {
       );
     }
 
-    // for (let i = 0; i < milestoneSeeds.length; i++) {
-    //   const { _id } = await Milestone.create(milestoneSeeds[i]);
-    //   const application = await Application.findOneAndUpdate(
-    //     {
-    //       $addToSet: {
-    //         milestones: _id,
-    //       },
-    //     }
-    //   );
-    // }
+    for (let i = 0; i < milestoneSeeds.length; i++) {
+      const { _id, appStone } = await Milestone.create(milestoneSeeds[i]);
+      
+      const application = await Application.findOneAndUpdate(
+        { _id: appStone },
+        {
+          $addToSet: {
+            milestones: _id,
+          },
+        }
+      );
+    }
+
+
   } catch (err) {
     console.error(err);
     process.exit(1);

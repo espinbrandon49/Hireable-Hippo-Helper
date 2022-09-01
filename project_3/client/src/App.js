e1484ff91cc4a97890f11e48e626dcfd30f12c41
Brice Veyna <bricev99@gmail.com>
2022-09-01 16:10:29 -0700

    Reconfigure to render TrackerContainer

--- project_3/client/src/App.js
+++ project_3/client/src/App.js
@@ -1,24 +1,7 @@
+import React from "react";
+import TrackerContainer from "./components/TrackerContainer";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.natgeokids.com/uk/discover/animals/general-animals/ten-hippo-facts/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Job Application Tracker Coming Soon!
-
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => <TrackerContainer />;
 
 export default App;

e6671cb8541e752cea82c93add09c7b87ce51da6
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-09-02 08:40:46 -0700

    add authLink middleware to apollo client for checking GraphQL requests

--- project_3/client/src/App.js
+++ project_3/client/src/App.js
@@ -14,6 +14,31 @@ import "./App.css";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+// https://www.apollographql.com/docs/react/api/link/apollo-link-context/
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+    // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
 const App = () => <TrackerContainer />;
 
 export default App;

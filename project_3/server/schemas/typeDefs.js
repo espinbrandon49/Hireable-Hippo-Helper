9b040cba6cafc9afb726b0ee59684e70215000ec
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-09-02 08:18:52 -0700

    add LOGIN_USER mutation typede f:

--- project_3/server/schemas/typeDefs.js
+++ project_3/server/schemas/typeDefs.js
@@ -80,4 +80,16 @@ const typeDefs = gql`
   }
 `;
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 module.exports = typeDefs;

6887bb6f7c1677c8f0ea1a5d3fec0406d84fc3ca
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-09-02 08:38:42 -0700

    import apollo client, router and signup/login pages to App.js

--- project_3/client/src/App.js
+++ project_3/client/src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import TrackerContainer from "./components/TrackerContainer";
-import './App.css';
+import "./App.css";
+
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 const App = () => <TrackerContainer />;
 

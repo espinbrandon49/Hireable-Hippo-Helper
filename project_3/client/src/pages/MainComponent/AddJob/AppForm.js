fea2368559b2f1a4de73f022febe238761e1a64b
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-09-02 10:53:51 -0700

    fix import folders

--- project_3/client/src/pages/MainComponent/AddJob/AppForm.js
+++ project_3/client/src/pages/MainComponent/AddJob/AppForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { checkForText, validateEmail } from '../../utils/helpers';
+import { checkForText, validateEmail } from '../../../utils/helpers';
 
 function ApplicationForm() {
 

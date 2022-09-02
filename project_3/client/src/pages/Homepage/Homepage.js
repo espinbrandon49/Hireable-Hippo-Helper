12e9ee5f79ee2fb24115df30123445a73c4d01c2
Jeffrey <jeffz98@berkeley.edu>
2022-09-02 16:08:43 -0700

    added login page

--- project_3/client/src/pages/Homepage/Homepage.js
+++ project_3/client/src/pages/Homepage/Homepage.js
@@ -3,15 +3,16 @@ import React from 'react'
 const Homepage = () => {
   return (
     <body>
-    <section class="hero is-medium">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-1 ">Hireable-Hippo-Helper</h1>
-          <h2 class="subtitle">We help young pottamus get hired!</h2>
-          <a href="learn more" class="button is-white is-medium is-inverted">Learn More&ensp;<i class="fad fa-chevron-right"></i></a>
+    <section className="hero is-medium">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title is-1 ">Hireable-Hippo-Helper</h1>
+          <h2 claclassNamess="subtitle">We help young pottamus get hired!</h2>
+          <a href="learn more" className="button is-white is-medium is-inverted">Learn More&ensp;<i className="fad fa-chevron-right"></i></a>
         </div>
       </div>
     </section>
+
     </body>
   )
 }

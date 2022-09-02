26bb54b36eab29d97dd5d23e5f89da734f10de65
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-09-02 08:46:18 -0700

    add context authMiddleware to apollo server

--- project_3/server/server.js
+++ project_3/server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 
-
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 
@@ -13,6 +13,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));

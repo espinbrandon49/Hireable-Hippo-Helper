11993ae3277cbaffb382c61240b53ef3d99356ee
Jeffrey <jeffz98@berkeley.edu>
2022-09-02 15:31:23 -0700

    began work on homepage

--- project_3/client/src/pages/Homepage/Homepage.js
+++ project_3/client/src/pages/Homepage/Homepage.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>
-      HOMEPAGE - Hello HippoWorld
-    </div>
+    <body>
+    <section class="hero is-medium">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1 ">Hireable-Hippo-Helper</h1>
+          <h2 class="subtitle">We help young pottamus get hired!</h2>
+          <a href="learn more" class="button is-white is-medium is-inverted">Learn More&ensp;<i class="fad fa-chevron-right"></i></a>
+        </div>
+      </div>
+    </section>
+    </body>
   )
 }
 

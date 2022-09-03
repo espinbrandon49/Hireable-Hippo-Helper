bb3c33ac1becb503daa317abe005fab69251e429
Jeffrey <jeffz98@berkeley.edu>
2022-09-03 09:47:58 -0700

    added bracket

--- client/src/pages/MainComponent/HeroSection/HeroSection.js
+++ client/src/pages/MainComponent/HeroSection/HeroSection.js
@@ -1,36 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import background from "../../../images/hippo-background.jpg";
 
 import Auth from "../../../utils/auth";
 
-
 function HeroSection() {
   return (
     <>
-      <div style={{ backgroundImage: `url(${background})` }} className="container">
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="container"
+      >
         <h1 className="title is-1 ">Hireable-Hippo-Helper</h1>
         <h2 className="subtitle">We help young pottamus get hired!</h2>
         <a href="learn more" class="button is-white is-medium is-inverted">
           Learn More&ensp;<i className="fad fa-chevron-right"></i>
         </a>
       </div>
-
-      <div>
       {Auth.loggedIn() ? (
         <>
-        <span>Hey there, {Auth.getProfile().data.username}!</span>
-      </>
+          <span>Hey there, {Auth.getProfile().data.username}!</span>
+        </>
       ) : (
         <div>
-        <Link to="/Signup">
-          <button type="button">Sign Up!</button>
-        </Link>
-      </div>
-
+          <Link to="/Signup">
+            <button type="button">Sign Up!</button>
+          </Link>
+        </div>
+      )}
     </>
-
   );
 }
 

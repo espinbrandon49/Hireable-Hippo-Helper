3ca2f90f15949cd669d12df769db18299df4c4da
Jeffrey <jeffz98@berkeley.edu>
2022-09-02 17:59:24 -0700

    added menu to hamburger

--- project_3/client/src/pages/Homepage/Homepage.js
+++ project_3/client/src/pages/Homepage/Homepage.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 const Homepage = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <>
     <nav className='navbar'>
@@ -9,6 +12,16 @@ const Homepage = () => {
         <Link to='/' className='navbar-logo'>
        <img alt="hippo" src="/images/hippoIcon.png"/>
         </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li classNames='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
     <body>

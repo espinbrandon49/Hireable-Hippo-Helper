104cdfd6fc855601e5ea9c90ba1f2b2b1c8fe330
Brice Veyna <bricev99@gmail.com>
2022-09-04 16:27:17 -0700

    Add individual application info using id passed from sidebar

--- client/src/pages/MainComponent/Application/components/allMilestones.js
+++ client/src/pages/MainComponent/Application/components/allMilestones.js
@@ -1,33 +1,69 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-const allMilestones = () => {
-  return (
-    <div className='column is-one-quarter'>
-      <div className='box'>
-        <h3 className='subtitle mb-2'>Job Information</h3>
-        <ul>
-          <li>Job Description: </li>
-          <li>Location: </li>
-          <li>Job Type: </li>
-          <li>Salary: </li>
-          <li>Link: </li>
-        </ul>
+const AllMilestones = ({applications}) => {
 
-        <h3 className='subtitle mb-2 mt-4'>Important Dates</h3>
-        <ul>
-          <li>Data Applied: </li>
-          <li>Interview Dates: </li>
-        </ul>
+  const { _id } = useParams();
 
-        <h3 className='subtitle mb-2 mt-4'>Contact Info</h3>
-        <ul>
-          <li>Contact Name: </li>
-          <li>Contact Email: </li>
-          <li>Contact Phone: </li>
-        </ul>
+  const application = applications.filter((application) => application._id === _id)[0];
+
+  if (!application) {
+    return (
+      <div className='column is-one-quarter'>
+        <div className='box'>
+          <h3 className='subtitle mb-2'>Job Information</h3>
+          <ul>
+            <li>Job Description:</li>
+            <li>Location:</li>
+            <li>Job Type:</li>
+            <li>Salary:</li>
+            <li>Link:</li>
+          </ul>
+  
+          <h3 className='subtitle mb-2 mt-4'>Important Dates</h3>
+          <ul>
+            <li>Data Applied:</li>
+            <li>Interview Dates:</li>
+          </ul>
+  
+          <h3 className='subtitle mb-2 mt-4'>Contact Info</h3>
+          <ul>
+            <li>Contact Name:</li>
+            <li>Contact Email:</li>
+            <li>Contact Phone:</li>
+          </ul>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className='column is-one-quarter'>
+        <div className='box'>
+          <h3 className='subtitle mb-2'>Job Information</h3>
+          <ul>
+            <li>Job Description: {application.jobDescription}</li>
+            <li>Location: </li>
+            <li>Job Type: {application.jobType}</li>
+            <li>Salary: {application.salary}</li>
+            <li>Link: {application.jobLink}</li>
+          </ul>
+  
+          <h3 className='subtitle mb-2 mt-4'>Important Dates</h3>
+          <ul>
+            <li>Data Applied: {application.dateApplied}</li>
+            <li>Interview Dates: </li>
+          </ul>
+  
+          <h3 className='subtitle mb-2 mt-4'>Contact Info</h3>
+          <ul>
+            <li>Contact Name: {application.contactName}</li>
+            <li>Contact Email: {application.contactEmail}</li>
+            <li>Contact Phone: {application.contactPhone}</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default allMilestones
+export default AllMilestones

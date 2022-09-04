82451d833febd022cfcb23d8a1ec1a4b735d8303
Brandon Espinosa <espinbrandon49@gmail.com>
2022-09-04 15:52:02 -0700

    create update application note functionality

--- client/src/pages/MainComponent/Application/components/notes.js
+++ client/src/pages/MainComponent/Application/components/notes.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState} from 'react'
+import Auth from "../../../../utils/auth";
 // import react-Quill
 import { useQuill } from 'react-quilljs';
 // import Quill stylesheet
 import 'quill/dist/quill.snow.css'
 
+import { useMutation } from '@apollo/client';
+import { UPDATE_NOTE } from "../../../../utils/mutations"
+
 const Notes = () => {
-
-  const [formState, setFormState] = useState({ notes: "" })
-
+  const [formState, setFormState] = useState({
+    applicant: Auth.getProfile().data.username,
+    notes: "" 
+  })
   // initiate  Quill
   const { quill, quillRef } = useQuill();
 
@@ -18,44 +23,55 @@ const Notes = () => {
     }
   }, [quill]);
 
-  // const handleFormSubmit = async (event) => {
-  //   event.preventDefault();
-  //   console.log(formState);
-  //     try {
-  //       const {data} = await updateApplication({
-  //         variables: {...formState, applicant: Auth.getProfile().data.username}
-  //       });
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  // };
-
   // useEffect hook to set the state using the text value in Quill=editor on change
   //resembles handleInputChange but more hip-po
   React.useEffect(() => {
     if (quill) {
       quill.on('text-change', (delta, oldDelta, source) => {
         // console.log('Text change!');
-        console.log(quill.getText()); // Get text only
+        console.log(typeof quill.getText()); // Get text only
         // console.log(quill.getContents()); // Get delta contents
         // console.log(quill.root.innerHTML); // Get innerHTML using quill
         // console.log(quillRef.current.firstChild.innerHTML); // Get innerHTML using quillRef
         setFormState({
+          applicant: Auth.getProfile().data.username,
           notes: quill.getText(),
         });
       });
     }
   }, [quill]);
 
-  console.log(formState)
+  const [updateNote, {data, loading, error }] = useMutation(UPDATE_NOTE);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+      try {
+        const {data} = await updateNote({
+          variables: {...formState}
+        });
+      } catch (error) {
+        console.error(error);
+      }
+  };
+
+  if (loading) return 'Updating';
+  if (error) return `Update error! ${error.message}`
 
   return (
-    <div className='box column' >
-      {/* Quill Wrapper contains toolbar and editor */}
-      <div style={{ width: 400, height: 200 }}>
+    <div className='box column'>
+      {/*Form wrapper contains toolbar, editor, submit btn */}
+      <form style={{ width: 400, height: 200 }} onSubmit={handleFormSubmit} >        
         {/* Quill editor  */}
         <div ref={quillRef} />
-      </div>
+        <button
+          className="button is-info"
+          style={{ cursor: "pointer" }}
+          name="submit"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   )
 }

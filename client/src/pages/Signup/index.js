01e3bfb298b5d339b5126849252eb2aa97f0a2aa
Brandon Espinosa <espinbrandon49@gmail.com>
2022-09-06 09:27:36 -0700

    style signup form

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -9,95 +9,164 @@ import NavTabs from '../NavBar';
 import Auth from '../../utils/auth';
 ///MERN 26-Stu_Resolver-Contect
 const Signup = () => {
-    const [formState, setFormState] = useState({
-      username: '',
-      email: '',
-      password: '',
-    });
-    const [addUser, { error, data }] = useMutation(ADD_USER);
-  
-    const handleChange = (event) => {
-      const { name, value } = event.target;
-  
-      setFormState({
-        ...formState,
-        [name]: value,
-      });
-    };
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [addUser, { error, data }] = useMutation(ADD_USER);
 
-    const handleFormSubmit = async (event) => {
-      event.preventDefault();
-      console.log(formState);
-  
-      try {
-        const { data } = await addUser({
-          variables: { ...formState },
-        });
-  
-        Auth.login(data.addUser.token);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-  
-    return (
-      <main className="flex-row justify-center mb-4">
-        <NavTabs/>
-        <div className="col-12 col-lg-10">
-          <div className="card">
-            <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
-            <div className="card-body">
-              {data ? (
-                <p>
-                  Success! You may now head{' '}
-                  <Link to="/">back to the homepage.</Link>
-                </p>
-              ) : (
-                <form onSubmit={handleFormSubmit}>
-                  <input
-                    className="form-input"
-                    placeholder="Your username"
-                    name="username"
-                    type="text"
-                    value={formState.name}
-                    onChange={handleChange}
-                  />
-                  <input
-                    className="form-input"
-                    placeholder="Your email"
-                    name="email"
-                    type="email"
-                    value={formState.email}
-                    onChange={handleChange}
-                  />
-                  <input
-                    className="form-input"
-                    placeholder="******"
-                    name="password"
-                    type="password"
-                    value={formState.password}
-                    onChange={handleChange}
-                  />
-                  <button
-                    className="btn btn-block btn-primary"
-                    style={{ cursor: 'pointer' }}
-                    type="submit"
-                  >
-                    Submit
-                  </button>
-                </form>
-              )}
-  
-              {error && (
-                <div className="my-3 p-3 bg-danger text-white">
-                  {error.message}
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <main className="flex-row justify-center mb-4">
+      <NavTabs />
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h3 className="card-header title p-3 mb-1">Signup</h3>
+          <div className="card-body">
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form className="p-6" onSubmit={handleFormSubmit}>
+
+                {/* <input
+                  className="form-input"
+                  placeholder="Your username"
+                  name="username"
+                  type="text"
+                  value={formState.name}
+                  onChange={handleChange}
+                /> */}
+
+                <div className='field'>
+                  <label htmlFor='username' className='label'>
+                    username
+                  </label>
+
+                  <div className='control'>
+                    <input
+                      className="input"
+                      placeholder="Your Username"
+                      name="username"
+                      type="text"
+                      value={formState.name}
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
-              )}
-            </div>
+
+                {/* <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />*/}
+
+                <div className='field'>
+                  <label htmlFor='email' className='label'>
+                    Email
+                  </label>
+
+                  <div className='control'>
+                    <input
+                      className="input"
+                      placeholder="Your email"
+                      name="email"
+                      type="email"
+                      value={formState.email}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+
+                {/* <input 
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+                /> */}
+
+                <div className='field'>
+                  <label htmlFor='password' className='label'>
+                    Password
+                  </label>
+
+                  <div className='control'>
+                    <input
+                      className="input"
+                      id='password'
+                      placeholder="********"
+                      name="password"
+                      type="password"
+                      value={formState.password}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+
+                {/* <button
+                  className="btn btn-block btn-primary"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button> */}
+
+                <div className='field pt-3'>
+                  <div className='control'>
+                    <button
+                      className="button is-info"
+                      style={{ cursor: 'pointer' }}
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
           </div>
         </div>
-      </main>
-    );
-  };
-  
-  export default Signup;
+      </div>
+    </main>
+  );
+};
+
+export default Signup;

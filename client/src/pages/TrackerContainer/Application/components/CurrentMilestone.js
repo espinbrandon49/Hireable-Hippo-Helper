40acffdeb5e24f39ec9a4b798e9caeb9cdb36fa3
Brice Veyna <bricev99@gmail.com>
2022-09-27 10:31:25 -0700

    Add new currentMilestones tracker

--- client/src/pages/TrackerContainer/Application/components/CurrentMilestone.js
+++ client/src/pages/TrackerContainer/Application/components/CurrentMilestone.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import { useMutation } from '@apollo/client';
 import { UPDATE_MILESTONE, ADD_MILESTONE } from '../../../../utils/mutations';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
+import ArticleIcon from '@mui/icons-material/Article';
+import PhoneIcon from '@mui/icons-material/Phone';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import PersonIcon from '@mui/icons-material/Person';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
 
 const CurrentMilestone = ({ application }) => {
 
@@ -8,10 +24,40 @@ const CurrentMilestone = ({ application }) => {
   const [addMilestone, { err, dataA }] = useMutation(ADD_MILESTONE);
   // console.log(error, err, data, dataA)
 
-  const submitMilestone = async (event) => {
-    event.preventDefault();
-    const currentMilestone = event.target.value;
-    const milestones = event.target.value
+  // Step Handling
+  const steps = ["Applied", "Phone Interview", "Technical Interview", "In Person Interview", "Job Offer", "Accepted"];
+
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [completed, setCompleted] = React.useState({});
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = async () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+    
+    const currentMilestone = steps[newActiveStep];
+    const milestones = steps[newActiveStep];
+    console.log(currentMilestone)
  
     try {
       await updateMilestone({
@@ -23,106 +69,267 @@ const CurrentMilestone = ({ application }) => {
     } catch (err) {
       console.error(err);
     }  
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  const handleComplete = () => {
+    for (let i = 0; i < application.milestones.length; i++) {
+      completed[steps.indexOf(application.milestones[i])] = true
+    }
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    handleNext();
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    setCompleted({});
+  };
+
+  // Button Colors and Icons
+  const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+      top: 22,
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        backgroundImage:
+          'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+      },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        backgroundImage:
+          'linear-gradient( 95deg,rgb(15,113,35) 0%,rgb(30,125,150) 50%,rgb(138,35,135) 100%)',
+      },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+      height: 3,
+      border: 0,
+      backgroundColor:
+        theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+      borderRadius: 1,
+    },
+  }));
+
+  const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc',
+    zIndex: 1,
+    color: '#fff',
+    width: 50,
+    height: 50,
+    display: 'flex',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...(ownerState.active && {
+      backgroundImage:
+        'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+      boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+    }),
+    ...(ownerState.completed && {
+      backgroundImage:
+        'linear-gradient( 136deg, rgb(15,113,35) 0%, rgb(30,125,150) 50%, rgb(138,35,135) 100%)',
+    }),
+  }));
+
+  function ColorlibStepIcon(props) {
+    const { active, completed, className } = props;
+  
+    const icons = {
+      1: <ArticleIcon />,
+      2: <PhoneIcon />,
+      3: <ManageAccountsIcon />,
+      4: <PersonIcon />,
+      5: <AttachMoneyIcon />,
+      6: <ThumbUpIcon />,
+    };
+  
+    return (
+      <ColorlibStepIconRoot ownerState={{ completed, active }} className={className}>
+        {icons[String(props.icon)]}
+      </ColorlibStepIconRoot>
+    );
   }
+
+
+
+  // const submitMilestone = async (event) => {
+  //   event.preventDefault();
+  //   const currentMilestone = event.target.value;
+  //   const milestones = event.target.value
+ 
+  //   try {
+  //     await updateMilestone({
+  //       variables: { _id: application._id, currentMilestone: currentMilestone },
+  //     });
+  //     await addMilestone({
+  //       variables: { _id: application._id, milestones: [milestones] },
+  //     });
+  //   } catch (err) {
+  //     console.error(err);
+  //   }  
+  // }
 
   if (application._id !== undefined) {
     return (
-      <div className='box'>
-      <div className="buttons is-justify-content-center">
-          <button
-            className={
-              application._id
-              ? application.currentMilestone === "Applied"
-                ? 'button is-primary'
-                : 'button is-info'
-              : 'button is-info'
-            }
-            value={"Applied"}
-            onClick={submitMilestone}
-          >
-            Applied
-          </button>
-          <button
-            className={
-              application._id
-              ? application.currentMilestone === "Phone Interview"
-                ? 'button is-link'
-                : 'button is-info'
-              : 'button is-info'
-            }
-            value={"Phone Interview"}
-            onClick={submitMilestone}
-          >
-            Phone Interview
-          </button>
-          <button
-            className={
-              application._id
-              ? application.currentMilestone === "Technical Interview"
-                ? 'button is-link'
-                : 'button is-info'
-              : 'button is-info'
-            }
-            value={"Technical Interview"}
-            onClick={submitMilestone}
-          >
-            Technical Interview
-          </button>
-  
-          <button
-            className={
-              application._id
-              ? application.currentMilestone === "In Person Interview"
-                ? 'button is-link'
-                : 'button is-info'
-              : 'button is-info'
-            }
-            value={"In Person Interview"}
-            onClick={submitMilestone}
-          >
-            In Person Interview
-          </button>
-          <button
-            className={
-              application._id
-              ? application.currentMilestone === "Job Offer"
-                ? 'button is-success'
-                : 'button is-info'
-              : 'button is-info'
-            }
-            value={"Job Offer"}
-            onClick={submitMilestone}
-          >
-            Job Offer
-          </button>
-          <button
-            className={
-              application._id
-              ? application.currentMilestone === "Accepted"
-                ? 'button is-success'
-                : 'button is-info'
-              : 'button is-info'
-            }
-            value={"Accepted"}
-            onClick={submitMilestone}
-          >
-            Accepted
-          </button>
-          <button
-            className={
-              application._id
-              ? application.currentMilestone === "Rejected"
-                ? 'button is-danger'
-                : 'button is-info'
-              : 'button is-info'
-            }
-            value={"Rejected"}
-            onClick={submitMilestone}
-          >
-            Rejected
-          </button>
+      <Box sx={{ width: '100%' }}>
+        <Stepper activeStep={activeStep} alternativeLabel connector={<ColorlibConnector />} sx={{ pt: 2 }}>
+          {steps.map((label, index) => (
+            <Step key={label} completed={completed[index]}>
+              <StepLabel StepIconComponent={ColorlibStepIcon} onClick={handleStep(index)}>
+                {label}
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <div>
+          {allStepsCompleted() ? (
+            <React.Fragment>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                All steps completed - you&apos;re finished
+              </Typography>
+              <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                <Box sx={{ flex: '1 1 auto' }} />
+                <Button onClick={handleReset}>Reset</Button>
+              </Box>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                <Button
+                  color="inherit"
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                  sx={{ mr: 1 }}
+                >
+                  Back
+                </Button>
+                <Box sx={{ flex: '1 1 auto' }} />
+                <Button onClick={handleNext} sx={{ mr: 1 }}>
+                  Next
+                </Button>
+                {activeStep !== steps.length &&
+                  (completed[activeStep] ? (
+                    <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                      Step {activeStep + 1} already completed
+                    </Typography>
+                  ) : (
+                    <Button onClick={handleComplete}>
+                      {completedSteps() === totalSteps() - 1
+                        ? 'Finish'
+                        : 'Complete Step'}
+                    </Button>
+                  ))}
+              </Box>
+            </React.Fragment>
+          )}
         </div>
-      </div>
+      </Box>
+      // <div className='box'>
+      // <div className="buttons is-justify-content-center">
+      //     <button
+      //       className={
+      //         application._id
+      //         ? application.currentMilestone === "Applied"
+      //           ? 'button is-primary'
+      //           : 'button is-info'
+      //         : 'button is-info'
+      //       }
+      //       value={"Applied"}
+      //       onClick={submitMilestone}
+      //     >
+      //       Applied
+      //     </button>
+      //     <button
+      //       className={
+      //         application._id
+      //         ? application.currentMilestone === "Phone Interview"
+      //           ? 'button is-link'
+      //           : 'button is-info'
+      //         : 'button is-info'
+      //       }
+      //       value={"Phone Interview"}
+      //       onClick={submitMilestone}
+      //     >
+      //       Phone Interview
+      //     </button>
+      //     <button
+      //       className={
+      //         application._id
+      //         ? application.currentMilestone === "Technical Interview"
+      //           ? 'button is-link'
+      //           : 'button is-info'
+      //         : 'button is-info'
+      //       }
+      //       value={"Technical Interview"}
+      //       onClick={submitMilestone}
+      //     >
+      //       Technical Interview
+      //     </button>
+  
+      //     <button
+      //       className={
+      //         application._id
+      //         ? application.currentMilestone === "In Person Interview"
+      //           ? 'button is-link'
+      //           : 'button is-info'
+      //         : 'button is-info'
+      //       }
+      //       value={"In Person Interview"}
+      //       onClick={submitMilestone}
+      //     >
+      //       In Person Interview
+      //     </button>
+      //     <button
+      //       className={
+      //         application._id
+      //         ? application.currentMilestone === "Job Offer"
+      //           ? 'button is-success'
+      //           : 'button is-info'
+      //         : 'button is-info'
+      //       }
+      //       value={"Job Offer"}
+      //       onClick={submitMilestone}
+      //     >
+      //       Job Offer
+      //     </button>
+      //     <button
+      //       className={
+      //         application._id
+      //         ? application.currentMilestone === "Accepted"
+      //           ? 'button is-success'
+      //           : 'button is-info'
+      //         : 'button is-info'
+      //       }
+      //       value={"Accepted"}
+      //       onClick={submitMilestone}
+      //     >
+      //       Accepted
+      //     </button>
+      //     <button
+      //       className={
+      //         application._id
+      //         ? application.currentMilestone === "Rejected"
+      //           ? 'button is-danger'
+      //           : 'button is-info'
+      //         : 'button is-info'
+      //       }
+      //       value={"Rejected"}
+      //       onClick={submitMilestone}
+      //     >
+      //       Rejected
+      //     </button>
+      //   </div>
+      // </div>
     )
   } else {
     return(
